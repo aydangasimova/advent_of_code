c4110fba353b9001dfaa101ef38584ca85a6e967
Gasimova, Aydan <aydan.gasimova@kpn.com>
2021-12-10 02:54:44 +0100

    buggy but complete logic is added to part 2 day 8

--- day8.py
+++ day8.py
@@ -30,27 +30,29 @@ class Digit:
 
 class Display:
     def __init__(self, displayed_string: str):
-        self.one = {'c', 'f'}
+        self.displayed_string = displayed_string
 
     def translator(self):
         pass
 
 
 class Decoder:
-    def __init__(self):
+    def __init__(self, puzzle):
+        self.puzzle = puzzle
         self.correct_letter_dict = {0: {'a', 'b', 'c', 'f', 'g'},
-                                   1: {'c', 'f'},
-                                   2: {'a', 'c', 'd', 'e', 'g'},
-                                   3: {'a', 'c', 'd', 'f', 'g'},
-                                   4: {'b', 'd', 'c', 'f'},
-                                   5: {'a', 'b', 'd', 'f', 'g'},
-                                   6: {'a', 'b', 'd', 'e', 'f', 'g'},
-                                   7: {'a', 'c', 'f'},
-                                   8: {'a', 'b', 'c', 'd', 'e', 'f', 'g'},
-                                   9: {'a', 'b', 'c', 'd', 'f', 'g'}}
+                                    1: {'c', 'f'},
+                                    2: {'a', 'c', 'd', 'e', 'g'},
+                                    3: {'a', 'c', 'd', 'f', 'g'},
+                                    4: {'b', 'd', 'c', 'f'},
+                                    5: {'a', 'b', 'd', 'f', 'g'},
+                                    6: {'a', 'b', 'd', 'e', 'f', 'g'},
+                                    7: {'a', 'c', 'f'},
+                                    8: {'a', 'b', 'c', 'd', 'e', 'f', 'g'},
+                                    9: {'a', 'b', 'c', 'd', 'f', 'g'}}
         self.decoder_dict = {}
         self.letter_dict = {}
         self.unsolved_letters = []
+        self.letter_translator = {}
 
     def add_digit_to_decoder_dict(self, digit, encoded_letters):
         self.decoder_dict[digit] = encoded_letters
@@ -59,6 +61,13 @@ class Decoder:
         for letter in encoded_letters:
             pass
         return 0
+
+    def get_letter_frequency(self):
+        for letter in puzzle[0]:
+            pass
+
+    def add_to_letter_translator(self, encoded_letter_string, actual_letter):
+        self.letter_tranlator[actual_letter] = encoded_letter_string
 
 
 def find_easy_encoded_digits(easy_digits, all_puzzles):
@@ -85,42 +94,19 @@ def parse_file(input_file: str) -> List[List[List[str]]]:
     return all_puzzles
 
 
+def get_letter_frequency(letter_list):
+    letter_frequency = {}
+    for letters in letter_list:
+        for letter in letters:
+            if letter in letter_frequency.keys():
+                letter_frequency[letter] += 1
+            else:
+                letter_frequency[letter] = 1
+
+    return letter_frequency
+
+
 if __name__ == '__main__':
-
-    all_puzzles = parse_file("test.csv")
-
-    all_digits = {d for d in range(0,10)}
-    easy_digits = {1, 4, 7, 8}
-    hard_digits = all_digits.difference(easy_digits)
-
-    answer_1 = find_easy_encoded_digits(easy_digits, all_puzzles)
-
-    for puzzle in all_puzzles:
-        signal_patterns = puzzle[0]
-        encoded_output = puzzle[1]
-        decoder = Decoder()
-        for signal_pattern in signal_patterns:
-            for easy_digit in easy_digits:
-
-                if len(signal_pattern) == Digit(easy_digit).letter_count_in_number:
-                    decoder.add_digit_to_decoder_dict(easy_digit, signal_pattern)
-
-        print(decoder.decoder_dict)
-
-    # if you know that 1 is "cf" and 7 is "acf" => get that up_horizontal_line = ~a~ => deduce up_horizontal_line in 8
-    # --- based on letter_freq
-    # you know ~a~ based on difference between 1 and 7
-    # knowing {'c': 8, 'a': 8, 'b': 6, 'g': 7, 'f': 9, 'd': 7, 'e': 3},
-    # you know ~b~ is the only one with 6 mentions
-    # you know ~e~ is the only one with 3 mentions
-    # you know ~f~ is the only one with 9 mentions
-    # knowing ~a~ you can deduce that ~c~ is the only other letter with 8
-    # knowing which letter is 4 and which letters are 'bcf' you can deduce what ~d~
-    # knowing ~d~ you know the remaining letter (with 7 freq) is ~g~
-    # ---
-
-
-    # you know 8 and you know 4
 
     correct_letter_dict = {0: {'a', 'b', 'c', 'f', 'g'},
                            1: {'c', 'f'},
@@ -133,17 +119,62 @@ if __name__ == '__main__':
                            8: {'a', 'b', 'c', 'd', 'e', 'f', 'g'},
                            9: {'a', 'b', 'c', 'd', 'f', 'g'}}
 
-    letter_frequency = {}
-    for letters in correct_letter_dict.values():
-        for letter in letters:
-            if letter in letter_frequency.keys():
-                letter_frequency[letter] += 1
-            else:
-                letter_frequency[letter] = 1
+    all_puzzles = parse_file("test.csv")
+
+    all_digits = {d for d in range(0,10)}
+    easy_digits = {1, 4, 7, 8}
+    hard_digits = all_digits.difference(easy_digits)
+
+    answer_1 = find_easy_encoded_digits(easy_digits, all_puzzles)
+
+    correct_letter_freq = get_letter_frequency(correct_letter_dict.values())
+
+    for puzzle in all_puzzles:
+        signal_patterns = puzzle[0]
+        encoded_output = puzzle[1]
+        encoded_letter_freq = get_letter_frequency(signal_patterns)
+        decoder = Decoder(puzzle)
+
+        for signal_pattern in signal_patterns:
+            for easy_digit in easy_digits:
+
+                if len(signal_pattern) == Digit(easy_digit).letter_count_in_number:
+                    decoder.add_digit_to_decoder_dict(easy_digit, signal_pattern)
+                # if you know that 1 is "cf" and 7 is "acf" => get that up_horizontal_line = ~a~ => deduce up_horizontal_line in 8
+                # you know ~a~ based on difference between 1 and 7
+                decoder.letter_translator["a"] = decoder.decoder_dict[7].difference(decoder.decoder_dict[1])
+
+            for actual_letter in correct_letter_freq.items():
+                for encoded_letter in encoded_letter_freq.items():
+                    if encoded_letter[1] in [6, 3, 9]:
+                        # knowing {'c': 8, 'a': 8, 'b': 6, 'g': 7, 'f': 9, 'd': 7, 'e': 3},
+                        # you know ~b~ is the only one with 6 mentions
+                        # you know ~e~ is the only one with 3 mentions
+                        # you know ~f~ is the only one with 9 mentions
+                        if encoded_letter[1] == actual_letter[1]:
+                            decoder.letter_translator[actual_letter[0]] = encoded_letter[0]
+                        elif (encoded_letter[1] == 8) and (encoded_letter[1] != 'a'):
+                            # knowing ~a~ you can deduce that ~c~ is the only other letter with 8
+                            decoder.letter_translator['c'] = encoded_letter[0]
+
+            # knowing which letter is 4 and which letters are 'bcf' you can deduce what ~d~
+            bcf = set([decoder.letter_translator[bcf] for bcf in ['b', 'c', 'f']])
+            decoder.letter_translator['d'] = decoder.decoder_dict[4].difference(bcf)
+
+            # knowing ~d~ you know the remaining letter (with 7 freq) is ~g~
+            for actual_letter in correct_letter_freq.items():
+                for encoded_letter in encoded_letter_freq.items():
+                    if (encoded_letter[1] == 7) and (encoded_letter[1] != 'd'):
+                        decoder.letter_translator['g'] = encoded_letter[0]
+
+        print(decoder.decoder_dict)
+        print(encoded_letter_freq)
+
+
+
+
 
     # answer_2 = simulate_faster(all_initial_timers, days_to_run=256)
-
-
 
     print('Answer 1: ', answer_1)
     # print('Answer 2: ', answer_2)
